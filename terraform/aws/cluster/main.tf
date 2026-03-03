fbc16a23ad9725f22cac5afc78225a9bf75344fc
Larry B <larrykbanks21@gmail.com>
2026-03-03 15:08:44 -0800

    no agents for now

--- terraform/aws/cluster/main.tf
+++ terraform/aws/cluster/main.tf
@@ -5,6 +5,14 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "lkb-main-s3-bucket"
+    key            = "cluster/terraform.tfstate"
+    region         = "us-west-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
