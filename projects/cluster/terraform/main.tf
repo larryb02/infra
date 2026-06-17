4c0b4bb6c312cd1699c5cb294855963394358cdf
Larry Banks <larrykbanks21@gmail.com>
2026-06-17 11:25:06 -0400

    compute for k8s

--- projects/cluster/terraform/main.tf
+++ projects/cluster/terraform/main.tf
@@ -23,11 +23,11 @@ data "aws_subnets" "default" {
   }
 }
 
-module "sg_k3s_server" {
+module "sg_k8s_server" {
   source  = "terraform-aws-modules/security-group/aws//modules/kubernetes-api"
   version = "~> 5.3"
 
-  name   = "k3s-server-${var.env}"
+  name   = "k8s-server-${var.env}"
   vpc_id = data.aws_vpc.default.id
 
   ingress_cidr_blocks = [data.aws_vpc.default.cidr_block]
@@ -37,17 +37,34 @@ module "sg_k3s_server" {
   }
 }
 
-module "k3s_server" {
+module "k8s_control_plane" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 6.4"
 
-  name                   = "k3s-server-${var.env}"
+  name                   = "control-plane-${var.env}"
   instance_type          = var.instance_type
   subnet_id              = data.aws_subnets.default.ids[0]
-  vpc_security_group_ids = [module.sg_k3s_server.security_group_id]
+  vpc_security_group_ids = [module.sg_k8s_server.security_group_id]
 
   tags = {
-    Role = "k3s-server"
+    Role = "control-plane"
+    Env  = var.env
+  }
+}
+
+module "k8s_worker" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 6.4"
+
+  for_each = toset(["0", "1"])
+
+  name                   = "control-plane-${each.key}-${var.env}"
+  instance_type          = var.instance_type
+  subnet_id              = data.aws_subnets.default.ids[0]
+  vpc_security_group_ids = [module.sg_k8s_server.security_group_id]
+
+  tags = {
+    Role = "control-plane"
     Env  = var.env
   }
 }
--- projects/cluster/terraform/outputs.tf
+++ projects/cluster/terraform/outputs.tf
@@ -1,13 +1,13 @@
 output "server_id" {
-  value = module.k3s_server.id
+  value = module.k8s_server.id
 }
 
 output "server_public_ip" {
-  value = module.k3s_server.public_ip
+  value = module.k8s_server.public_ip
 }
 
 output "security_groups" {
   value = {
-    k3s_server = module.sg_k3s_server.security_group_id
+    k3s_server = module.sg_k8s_server.security_group_id
   }
 }
